1de9f6682599c0f322ea16439a57d9cc116c44fb
Linh <71812422+AlE-dip@users.noreply.github.com>
2023-08-09 21:03:24 +0700

    .

---
+++ src/main/java/com/ale/anotation/DefaultMethod.java
@@ -0,0 +1,9 @@
+package com.ale.anotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface DefaultMethod {
+}
---
+++ src/main/java/com/ale/anotation/User.java
@@ -0,0 +1,8 @@
+package com.ale.anotation;
+
+@DefaultMethod
+public class User {
+    private String username;
+    private String password;
+
+}
